eadce697889510f2b482569dc3f98a87ae03561e
marineco <marinecolonge@gmail.com>
2017-01-18 12:30:52 +0100

    erreur instruction, création méthode buy()

--- client.php
+++ client.php
@@ -4,12 +4,9 @@
 
 	class Client extends User {
 		
-		public function __construct($mail, $id, $date) {
-			parent::setEmail($mail);
-			parent::setId($id);
-			parent::setCreatedAt($date);
-		}
+		public function buy() {
 
+		}
 	}
 
 ?>
