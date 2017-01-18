d6dc1b5d2d55ad45b60ac16949861f30d4e7e2cd
marineco <marinecolonge@gmail.com>
2017-01-18 21:50:22 +0100

    ajout des accesseurs pour les 2 propriétés

--- client.php
+++ client.php
@@ -7,8 +7,22 @@
 		private $billAmount;
 		private $cart = [];
 
+		public function getBillAmount() {
+			return $this->billAmount; 
+		}
+		public function setBillAmount($billAmount) {
+			$this->billAmount = $billAmount;
+		}
+
+		public function getCart() {
+			return $this->cart; 
+		}
+		public function setCart($cart) {
+			$this->cart = $cart;
+		}
+
 		public function buy() {
-			
+
 		}
 	}
 
