6cf8d8da3d53270df88b27ad2da35aa312219117
marineco <marinecolonge@gmail.com>
2017-01-18 09:41:02 +0100

    ajout constructeur de la class Client qui hérite de la class User

--- client.php
+++ client.php
@@ -2,6 +2,11 @@
 
 	class Client extends User {
 		
+		public function __construct($string, $string, $string) {
+			parent::setEmail($string);
+			parent::setId($string);
+			parent::setCreatedAt($string);
+		}
 
 	}
 
