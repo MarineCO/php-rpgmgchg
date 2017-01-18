1de7822cfaa31ef992bb824fa86f25e3c6797291
marineco <marinecolonge@gmail.com>
2017-01-18 16:09:20 +0100

    ajout getters et setters des propriétés

--- vegetable.php
+++ vegetable.php
@@ -1,14 +1,45 @@
 <?php
 
-	require ('product.php');
+	require_once ('product.php');
 
 	class Vegetable extends Product {
 
 		private $productorName;
 		private $harvestedAt;
 
+		public function __construct($strIid, $strName, $strPrice, $Productor, $Harvested) {
+			$this->productorName = $Productor;
+			$this->harvestedAt = $Harvested;
+			parent::__construct($strIid, $strName, $strPrice)
+
+		}
+
+		public function getProductorName() {
+			return $this->productorName; 
+		}
+		public function setProductorName($Productor) {
+			$this->productorName = $Productor;
+		}
+
+		public function getHarvestedAt() {
+			return $this->harvestedAt; 
+		}
+		public function setHarvestedAt($Harvested) {
+			$this->harvestedAt = $Harvested;
+		}
+
 		public function isFresh() {
-			
+
+			$dateNow = time();
+			//tps courant - 1 jour (=24h)
+			$soFresh = time() - (strtotime('-1 day'));
+
+			if ($dateNow - strtotime($harvestedAt) < $soFresh) {
+				return true;
+			} else {
+				return false;
+			}
+
 		}
 
 	}
