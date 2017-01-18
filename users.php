90d7501bc3482fb341d8b4d4f8687fdf81ac85b5
marineco <marinecolonge@gmail.com>
2017-01-18 11:18:50 +0100

    modif dernière propriété date en une date php

--- users.php
+++ users.php
@@ -1,11 +1,14 @@
 <?php
 
-	$client1 = new Client('client1@gmail.com', '1', '15-01-17');
-	$client2 = new Client('client2@gmail.com', '2', '18-01-17');
+	require ('client.php');
+
+	$client1 = new Client('client1@gmail.com', 'id 1', date('d-m-Y'));
+	$client2 = new Client('client2@gmail.com', 'id 2', date('d-m-Y'));
 
 	return [
 		1 => $client1,
 		2 => $client2
 	];
+
 
 ?>
