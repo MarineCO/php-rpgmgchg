17839b2eb2a9344239353d9781b57f6634aada3b
marineco <marinecolonge@gmail.com>
2017-01-18 17:07:19 +0100

    modif du constructeur, ajout setter et getter, modif lien vers fichier product afin de ne l'appeler qu'une fois

--- cloth.php
+++ cloth.php
@@ -1,12 +1,20 @@
 <?php
 
-	require ('product.php');
+	require_once ('product.php');
 
 	class Cloth extends Product {
 		
 		private $brand;
 
-		public function __construct($Brand) {
+		public function __construct($strId, $strName, $strPrice, $Brand) {
+			$this->brand = $Brand;
+			parent::__construct($strId, $strName, $strPrice);
+		}
+
+		public function getBrand() {
+			return $this->brand; 
+		}
+		public function setBrand($Brand) {
 			$this->brand = $Brand;
 		}
 
