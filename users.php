b5e404461b822e4bfe576dc74a6215720f3ac204
marineco <marinecolonge@gmail.com>
2017-01-18 09:55:20 +0100

    création de deux nouvelles instances de Client et renvoi d'un tableau php de ces clients

---
+++ users.php
@@ -0,0 +1,11 @@
+<?php
+
+	$client1 = new Client('client1@gmail.com', '1', '15-01-17');
+	$client2 = new Client('client2@gmail.com', '2', '18-01-17');
+
+	return [
+		1 => $client1,
+		2 => $client2
+	];
+
+?>
