843203052491024b5b012e174c0058a072b3ed43
marineco <marinecolonge@gmail.com>
2017-01-18 11:54:33 +0100

    ajout du constructeur, des getters et des setters

--- product.php
+++ product.php
@@ -6,6 +6,34 @@
 		private $name;
 		private $price;
 
+		public function __construct($strIid, $strName, $strPrice) {
+			$this->id = $strId;
+			$this->name = $strName;
+			$this->price = $strPrice;
+		}
+
+		public function getId() {
+			return $this->id; 
+		}
+		public function setId($strIid) {
+			$this->id = $strId;
+		}
+
+		public function getName() {
+			return $this->name;
+		}
+		public function setName($strName) {
+			$this->name = $strName;
+		}
+
+		public function getPrice() {
+			return $this->price;
+		}
+		public function setPrice($strPrice) {
+			$this->price = $strPrice;
+		}
+
+
 	}
 
 ?>
