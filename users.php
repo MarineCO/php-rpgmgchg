2c1c5de99c6a0404a0889511948e200d5b7fb9ba
marineco <marinecolonge@gmail.com>
2017-01-18 17:15:23 +0100

    modif affichage du 3e argument

--- users.php
+++ users.php
@@ -2,8 +2,8 @@
 
 	require ('client.php');
 
-	$client1 = new Client('client1@gmail.com', 'id 1', date('d-m-Y'));
-	$client2 = new Client('client2@gmail.com', 'id 2', date('d-m-Y'));
+	$client1 = new Client('client1@gmail.com', 'id 1', date('d-m-Y', time()));
+	$client2 = new Client('client2@gmail.com', 'id 2', date('d-m-Y', time()));
 
 	return [
 		1 => $client1,
