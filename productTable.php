d2314d8b632ac910f3c7d38aad749bd7e7f025ad
marineco <marinecolonge@gmail.com>
2017-01-18 17:11:05 +0100

    initial

---
+++ productTable.php
@@ -0,0 +1,27 @@
+<?php
+
+	$products = require ('products.php');
+
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Exercice POO</title>
+</head>
+<body>
+	
+	<h1>Tableau produits :</h1>
+	
+	<table>
+		<tr>
+			<td>Id</td>
+			<td>Nom</td>
+			<td>Prix</td>
+			<td>Producteur</td>
+			<td>Date récolte</td>
+			<td>Marque</td>
+		</tr>
+	</table>
+	
+</body>
+</html>
