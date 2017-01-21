8e5bbf664091728aae4e679949c00e9f9dc23dd5
marineco <marinecolonge@gmail.com>
2017-01-21 14:51:58 +0100

    changement nom produits

--- products.php
+++ products.php
@@ -3,12 +3,12 @@
 	require_once ('vegetable.php');
 	require_once ('cloth.php');
 
-	$vegetable1 = new Vegetable('Id 1', 'Framboise', '3 euros', 'Prod 1', '10-01-2017');
-	$vegetable2 = new Vegetable('Id 2', 'Clémentine', '2 euros', 'Prod 2', '18-01-2017');
+	$vegetable1 = new Vegetable('Id 1', 'Poivron', '3 €', 'Prod 1', '10-01-2017');
+	$vegetable2 = new Vegetable('Id 2', 'Salade', '2 €', 'Prod 2', '18-01-2017');
 
-	$cloth1 = new Cloth('Id 3', 'Veste', '50 euros', 'Morgan');
-	$cloth2 = new Cloth('Id 4', 'Echarpe', '15 euros', 'Bonobo');
-	$cloth3 = new Cloth('Id 5', 'Robe', '70 euros', 'Mango');
+	$cloth1 = new Cloth('Id 3', 'Veste', '50 €', 'Morgan');
+	$cloth2 = new Cloth('Id 4', 'Echarpe', '15 €', 'Bonobo');
+	$cloth3 = new Cloth('Id 5', 'Robe', '70 €', 'Mango');
 
 	return [
 		1 => $vegetable1,
