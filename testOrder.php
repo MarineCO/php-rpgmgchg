51b1541f2220a391e82c54198e8da088a7751cb8
marineco <marinecolonge@gmail.com>
2017-01-21 15:15:25 +0100

    initial

---
+++ testOrder.php
@@ -0,0 +1,15 @@
+<?php
+
+	$users = require ('users.php');
+	$products = require ('products.php');
+
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Exercice POO</title>
+</head>
+<body>
+
+</body>
+</html>
